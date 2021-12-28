03052a74173165a23c5a4d6b4dfa8898db3baebe
Daniil <orgfrank@icloud.com>
2021-12-28 15:44:20 +0600

    Разгрузил gamescene

--- WarFly/GameScene.swift
+++ WarFly/GameScene.swift
@@ -7,29 +7,21 @@
 
 import SpriteKit
 import GameplayKit
-import CoreMotion
+
 
 class GameScene: SKScene {
     
-    var player: SKSpriteNode!
-    let motionManager = CMMotionManager()
-    var xAcceleration: CGFloat = 0
+    var player: PlayerPlane!
     
     override func didMove(to view: SKView) {
         configurateStarsScene()
         spawnCloud()
         spawnIsland()
+        player.performFly()
     }
     
     override func didSimulatePhysics(){
-        super.didSimulatePhysics()
-        player.position.x += xAcceleration * 50
-        
-        if player.position.x < -70 {
-            player.position.x = self.size.width + 70
-        } else if player.position.x > self.size.width + 70{
-            player.position.x = -70
-        }
+        player.checkPosition()
         
         enumerateChildNodes(withName: "backgroundSprite") { node, stop in
             if node.position.y < -199 {
@@ -54,14 +46,6 @@ class GameScene: SKScene {
         
         player = PlayerPlane.populate(at: CGPoint(x: screen.size.width / 2, y: 100))
         self.addChild(player)
-        
-        motionManager.accelerometerUpdateInterval = 0.2
-        motionManager.startAccelerometerUpdates(to: OperationQueue.current!) { (data, error) in
-            if let data = data {
-                let acceleration = data.acceleration
-                self.xAcceleration = CGFloat(acceleration.x ) * 0.7 + self.xAcceleration * 0.3
-            }
-        }
     }
     
     fileprivate func spawnCloud(){
